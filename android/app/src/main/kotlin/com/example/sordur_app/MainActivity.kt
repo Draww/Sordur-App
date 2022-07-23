b7efa83cdc690ff93c99a1a1c8b1ad7842a3c768
Draww <drawwbeta@gmail.com>
2022-07-23 17:43:43 +0300

    Init project files

---
+++ android/app/src/main/kotlin/com/example/sordur_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sordur_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
